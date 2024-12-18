ef243b81fd4f71f69eb7bbef476b71e66e0245e5
diptitate <diptitate7@gmail.com>
2024-12-18 12:24:30 +0530

    mongourl details

--- server/config/db.js
+++ server/config/db.js
@@ -1,24 +1,14 @@
-//added mongoose
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        const mongoURI = process.env.MONGO_URI || "mongodb+srv://ajvwebapp:ajvwebapp123@cluster0.rrsuh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-        if (!mongoURI) {
-            throw new Error("MongoDB URI is not defined.");
-        }
-
-        const conn = await mongoose.connect(mongoURI, {
-            // Deprecated options removed
-        });
-
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`MongoDB connection error: ${error.message}`);
-        process.exit(1); // Exit with failure
-    }
+    mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('MongoDB connected!', mongoose.connection.host);
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+    });
 };
 
 module.exports = connectDB;
-
 
