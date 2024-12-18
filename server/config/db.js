dc6e8fc498315d7a0a0aad6adf83e535ec2962a3
diptitate <diptitate7@gmail.com>
2024-12-18 11:35:27 +0530

    db changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log('MongoDB connected!', mongoose.connection.host);
-    })
-    .catch((err) => {
-        console.error('MongoDB connection error:', err);
-    });
+    try {
+        // Replace with your MongoDB connection string
+        const mongoURI = "mongodb+srv://ajvwebapp:ajvwebapp123@cluster0.rrsuh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+        const conn = await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1); // Exit process with failure
+    }
 };
 
 module.exports = connectDB;
