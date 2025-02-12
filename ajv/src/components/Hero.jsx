834f2ded84ff32af2e2dd5b8ab0f8b4a8a4e54c3
diptitate <diptitate7@gmail.com>
2025-02-12 17:47:21 +0530

    banner text aligned

--- ajv/src/components/Hero.jsx
+++ ajv/src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = (props) => {
       />
       <div className='bg-black/30 absolute top-0 left-0 w-full' />
       <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white'>
-        <div className='sm: top-[20%] md:left-[10%] max-w-[1100px] m-auto absolute p-4'>
+        <div className='lg: left-[8%] sm: top-[20%] md:left-[10%] max-w-[1100px] m-auto absolute p-4'>
           <h1 className='font-bold text-4xl md:text-4xl drop-shadow-2xl'>
             {props.title}
           </h1>
