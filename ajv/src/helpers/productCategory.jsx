f48de1694f9ae711ec9e7bc453053eac8c4ebada
diptitate <35255263+diptitate@users.noreply.github.com>
2025-02-20 09:21:39 +0530

    Delete ajv directory

--- ajv/src/helpers/productCategory.jsx
+++
@@ -1,8 +0,0 @@
-const productCategory = [
-    { id : 1, label : "Electric Bike", value : "electricbike"},
-    { id : 2, label : "Electric Car", value : "electriccar"},
-   
-]
-
-
-export default productCategory
